bcc35c5b1c8caadce6ccc449e60b0c114419e6a0
blupine <skwint11@gmail.com>
2020-12-31 14:02:16 +0900

    [Add] 실전예제 2번 - 양방향 연관관계

---
+++ ch6-exercise/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ORDERS") // DB마다 "ORDER"라는 이름을 다른 용도로 사용할 때가 있어서
+public class Order {
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @ManyToOne // 연관관계의 주인
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order") // 양방향 연관관계
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING) // EnumType.ORDINAL은 쓰면 안된다
+    private OrderStatus status;
+
+    public void addOrderItem(OrderItem orderItem){ // 연관관계 편의 메소드
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
