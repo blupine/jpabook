4ed0b049e6626457f99e02b6769d74c2d354e2d8
blupine <skwint11@gmail.com>
2021-01-01 18:51:32 +0900

    Add Book entity example code

--- ch7-exercise/src/main/java/jpabook/jpashop/domain/Book.java
+++ ch7-exercise/src/main/java/jpabook/jpashop/domain/Book.java
@@ -6,4 +6,12 @@ import javax.persistence.Entity;
 public class Book extends Item{
     private String author;
     private String isbn;
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
