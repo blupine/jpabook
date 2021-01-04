e8bd00dbcac1863d035d348a4a38700b110292f1
blupine <skwint11@gmail.com>
2021-01-04 19:12:37 +0900

    Add ch9-exercise codes

---
+++ ch9-exercise/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,65 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ORDERS") // DB마다 "ORDER"라는 이름을 다른 용도로 사용할 때가 있어서
+public class Order extends BaseEntity{
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY) // 연관관계의 주인
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL) // 지연 로딩, 영속성 전이
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // 양방향 연관관계 // 영속성 전이
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING) // EnumType.ORDINAL은 쓰면 안된다
+    private OrderStatus status;
+
+
+
+    public void addOrderItem(OrderItem orderItem){ // 연관관계 편의 메소드
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
